4a38f892a8413ce0b417223fa28f96c56feee2f1
shojol021 <shojol021@gmail.com>
2023-04-14 01:24:34 +0600

    dynamically loaded data on the server

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const products = require('./data.json')
+const { param } = require('express/lib/request')
 const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-    res.send('Hello World 2')
+    res.send('Hello World 5')
 })
 
 app.get('/next', (rq, rs) => {
-    rs.send('From Next Page')
+    rs.send('From Next Page 4')
+})
+
+app.get('/products', (rq,rs) => {
+    rs.send(products)
+})
+
+app.get('/products/:id', (req, res) => {
+    const id = req.params.id
+    const product = products.find(product => product.id == id) || "Didn't find any porduct"
+    res.send(product)
+
 })
 
 app.listen(port, () => {
